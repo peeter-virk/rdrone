644b7b41d90db0453aebb070bdcb5e7df5428969
visual_processing_pc <peetervirk@gmail.com>
2021-11-19 15:32:51 +0200

    ar tracking

---
+++ scripts/ar_marker_printout.py
@@ -0,0 +1,26 @@
+import rospy
+from visualization_msgs.msg import Marker
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+
+
+def marker_callback(data):
+	
+	print("pose")
+	print(data.pose.position)
+	print("rotation")
+	orientation = list(euler_from_quaternion([data.pose.orientation.x,data.pose.orientation.y,data.pose.orientation.z,data.pose.orientation.w]))
+	print("X: ", orientation[0])
+	print("Y: ", orientation[1])
+	print("Z: ", orientation[2])
+	#c_rot = list(euler_from_quaternion (orientation_list))
+		
+	#print(c_pose, c_rot)
+	
+	pass
+	
+
+if __name__ == "__main__":
+	rospy.init_node('m_printer', anonymous=True)
+	rospy.Subscriber("/visualization_marker", Marker , marker_callback)
+	while not rospy.is_shutdown():
+		pass
